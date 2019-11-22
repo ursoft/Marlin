96468b960cd0607cdc168c3f9afe13b3d4943052
Юрий Першин <ursoft2004@mail.ru>
2019-11-22 05:52:32 +0500

    #17 partial revert

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1365,7 +1365,11 @@
  * See http://marlinfw.org/docs/features/lin_advance.html for full instructions.
  * Mention @Sebastianv650 on GitHub to alert the author of any issues.
  */
-#define LIN_ADVANCE
+#if (GITHUB_USER==URSOFT)
+ #define LIN_ADVANCE
+#else
+ //#define LIN_ADVANCE //conflicts with stealthChop (UltiSteel stock) mode of TMC2208 (E stops extruding at ~10 layer)
+#endif
 #if ENABLED(LIN_ADVANCE)
   //#define EXTRA_LIN_ADVANCE_K // Enable for second linear advance constants
   #define LIN_ADVANCE_K 0       // Unit: mm compression per 1mm/s extruder speed
